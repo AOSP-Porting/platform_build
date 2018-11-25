68d7a65353566efb79c05383bd7c260158377fa9
Alberto97 <albertop2197@gmail.com>
2018-11-25 12:56:05 +0100

    Include BoardConfigExtra
    Change-Id: I8b299b69e95dc4a061a0b53c9afbe5d0fefe21d5

--- core/config.mk
+++ core/config.mk
@@ -223,6 +223,7 @@ include $(BUILD_SYSTEM)/envsetup.mk
 # Pruned directory options used when using findleaves.py
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
+include vendor/aosp/config/BoardConfigExtra.mk
 
 # The build system exposes several variables for where to find the kernel
 # headers:
